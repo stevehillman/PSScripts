389f7983a0ca4756f371baf8efd0de097ef52d68
Steve Hillman <hillman@sfu.ca>
2017-11-27 15:38:22 -0800

    Put @sfu.ca before +tag, not after!

--- enable-beforemigration.ps1
+++ enable-beforemigration.ps1
@@ -3,12 +3,12 @@
 # Make a user's mailbox visible to the Cloud Migrator prior to the start of migration
 
 $userid = '[!user-importname]'
-$userid = $userid+"@sfu.ca"
+$scopeduserid = $userid+"@sfu.ca"
 
-$mb = Get-Mailbox $userid -ErrorAction Stop
+$mb = Get-Mailbox $scopeduserid -ErrorAction Stop
 if ($mb.PrimarySmtpAddress -Match "\+sfu_connect")
 {
 	# User needs to be renamed prior to migration
-	$emailAddresses = @("SMTP:$userid","smtp:${userid}+sfu_connect")
-	Set-Mailbox $userid -EmailAddresses $emailAddresses -ErrorAction Stop
+	$emailAddresses = @("SMTP:${userid}@sfu.ca","smtp:${userid}+sfu_connect@sfu.ca")
+	Set-Mailbox $scopeduserid -EmailAddresses $emailAddresses -ErrorAction Stop
 }
