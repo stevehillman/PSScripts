e81f8cc0a1b3c6fee10cce947616fb5a9143e870
Steve Hillman <hillman@sfu.ca>
2017-12-01 14:50:41 -0800

    Include changes needed to actually work in CM365

--- disable-aftermigration.ps1
+++ disable-aftermigration.ps1
@@ -1,6 +1,16 @@
 # Disable-aftermigration
 #
-# Make a user's mailbox visible to the Cloud Migrator prior to the start of migration
+# Hide a user's mailbox after migration completes. Needs to run
+# regardless of success or failure
+
+# Set up new PSSession first
+
+# Set this so that errors are thrown from Cmdlets
+$ErrorActionPreference = 'Stop'
+# Open the connection
+$sesh = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri [!ps-url] -credential (New-object -TypeName System.Management.Automation.PSCredential -argumentlist '[!admin-email]',(ConvertTo-SecureString '[!admin-password]' -AsPlainText -Force)) -Authentication Kerberos -AllowRedirection
+# Import the session
+$imp = Import-PSSession -Session $sesh -AllowClobber
 
 $userid = '[!user-importname]'
 $scopeduserid = $userid+"@sfu.ca"
@@ -8,6 +18,10 @@ $scopeduserid = $userid+"@sfu.ca"
 $mb = Get-Mailbox $scopeduserid -ErrorAction Stop
 if ($mb.EmailAddresses -contains "smtp:${userid}+sfu_connect@sfu.ca")
 {
-	# User needs to be renamed after migration	
-	Set-Mailbox $scopeduserid -EmailAddresses "${userid}+sfu_connect@sfu.ca" -ErrorAction Stop
+    # User needs to be renamed after migration  
+    Set-Mailbox $scopeduserid -EmailAddresses "${userid}+sfu_connect@sfu.ca" -AuditEnabled $true -ErrorAction Stop
 }
+
+# tear down the session when we're done
+
+Remove-PSSession -Session $sesh
--- enable-beforemigration.ps1
+++ enable-beforemigration.ps1
@@ -2,13 +2,27 @@
 #
 # Make a user's mailbox visible to the Cloud Migrator prior to the start of migration
 
+# Create session first
+
+# Set this so that errors are thrown from Cmdlets
+$ErrorActionPreference = 'Stop'
+# Open the connection
+$sesh = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri [!ps-url] -credential (New-object -TypeName System.Management.Automation.PSCredential -argumentlist '[!admin-email]',(ConvertTo-SecureString '[!admin-password]' -AsPlainText -Force)) -Authentication Kerberos -AllowRedirection
+# Import the session
+$imp = Import-PSSession -Session $sesh -AllowClobber
+
 $userid = '[!user-importname]'
 $scopeduserid = $userid+"@sfu.ca"
 
 $mb = Get-Mailbox $scopeduserid -ErrorAction Stop
 if ($mb.PrimarySmtpAddress -Match "\+sfu_connect")
 {
-	# User needs to be renamed prior to migration
-	$emailAddresses = @("SMTP:${userid}@sfu.ca","smtp:${userid}+sfu_connect@sfu.ca")
-	Set-Mailbox $scopeduserid -EmailAddresses $emailAddresses -ErrorAction Stop
+    # User needs to be renamed prior to migration
+        # Also disable auditing to spare the audit log the verbosity
+    $emailAddresses = @("SMTP:${userid}@sfu.ca","smtp:${userid}+sfu_connect@sfu.ca")
+    Set-Mailbox $scopeduserid -EmailAddresses $emailAddresses -AuditEnabled $false -ErrorAction Stop
 }
+
+# When we're done, tear down the session
+
+Remove-PSSession -Session $sesh
