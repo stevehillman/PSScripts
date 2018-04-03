6f37b5b0190866e8b7bdd1303481944925b91d82
Steve Hillman <hillman@sfu.ca>
2018-04-03 09:53:40 -0700

    Use AllowClobber on Exchange session so we can rerun script

--- import-allusers.ps1
+++ import-allusers.ps1
@@ -98,12 +98,12 @@ try {
         {
             # For testing..
             $Cred = Get-Credential
-            $ESession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $e_uri  -Authentication Kerberos -Credential $Cred
+            $ESession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $e_uri  -Authentication Kerberos -Credential $Cred -AllowClobber
         }
         else
         {
             # Prod
-            $ESession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $e_uri  -Authentication Kerberos
+            $ESession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $e_uri  -Authentication Kerberos -AllowClobber
         }
         # If the Import fails, it's probably just because the cmdlets have already been imported
         import-pssession $ESession -AllowClobber
